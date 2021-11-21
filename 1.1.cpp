2655bbbf9a44039bd990b089fbbb8bff3b7c4647
Ярослава Маслова <yaroslava.maslova.02@mail.ru>
2021-11-21 21:00:12 +0300

    1,1 изменения

--- 1.1.cpp
+++ 1.1.cpp
@@ -5,14 +5,13 @@ int main()
 {
 double x, y, z, a, b, e;
  e = 2.718281828459045;
-std::cout << "Введите значение x: ";
+std::cout << "Введите значение x: "; // просим ввести значения из таблицы//
 std::cin >> x;
 std::cout << "Введите значение y: ";
 std::cin >> y;
 std::cout << "Введите значение z: ";
 std::cin >> z;
-a =y * z *pow(x,2)-z/pow(sin(x/z),2);
+a =y * z *pow(x,2)-z/pow(sin(x/z),2); // находим x и y по формулам //
 b = z * cos(y*x/z)/pow(e,sqrt(z));
-std::cout<<"значение А= " <<a << std::endl;
-std::cout<<"значение B= " <<b;
+std::cout<<"значение А= " <<a << std::endl<<"значение B= " <<b; //выводим получившиеся значения//
 }
