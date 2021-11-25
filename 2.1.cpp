61f1b77360c275cb85cdcad1b36a057d6774c0c5
Ярослава Маслова <yaroslava.maslova.02@mail.ru>
2021-11-25 10:37:20 +0300

    2,1 исправлено

--- 2.1.cpp
+++ 2.1.cpp
@@ -8,7 +8,7 @@ std::cout << "Введите первое число: "; //просим ввес
 std::cin >> first ;
 std::cout << "Введите второе число: ";
 std::cin >> second;
-a = (pow(first,3)+pow(second,3))/2;//находим среднее арифметическое кубов//
+a = (pow(first,3)+pow(second,3))/2;//находим среднее арифметическое кубов //
 first = fabs(first);//находим модули чисел//
 second = fabs(second);
 b =(first + second)/2;//находим среднее число модулей //
