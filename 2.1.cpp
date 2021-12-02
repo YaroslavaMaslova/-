731626e7785bccbdb26a467fd08542fcfa1705a0
Ярослава Маслова <yaroslava.maslova.02@mail.ru>
2021-12-02 12:30:08 +0300

    2,1 исправлено

--- 2.1.cpp
+++ 2.1.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#define _USE_MATH_DEFINES // for C++
 #include <cmath>
 int main()
-{
-double first, second, a, b;
-std::cout << "Введите первое число: "; //просим ввести оба числа//
-std::cin >> first ;
-std::cout << "Введите второе число: ";
-std::cin >> second;
-a = (pow(first,3)+pow(second,3))/2;//находим среднее арифметическое кубов //
-first = fabs(first);//находим модули чисел//
-second = fabs(second);
-b =(first + second)/2;//находим среднее число модулей //
-std::cout<<"среднее арифметическое кубов этих чисел =" <<a<< std::endl<<"среднее геометрическое модулей этих чисел " <<b; //выводим получившиеся значения//
+{ 
+    std::cout << "Введите первое число: ";
+    double first;
+    std::cin >> first ;
+    std::cout << "Введите второе число: ";
+    double second;
+    std::cin >> second;
+    double arithmetic_mean_of_cubes = (pow(first,3)+pow(second,3))/2;
+    first = fabs(first);
+    second = fabs(second);
+    double average_of_modules =(first + second)/2;//находим среднее число модулей //
+    std::cout<<"среднее арифметическое кубов этих чисел =" <<arithmetic_mean_of_cubes<< std::endl<<"среднее геометрическое модулей этих чисел " <<average_of_modules;
 }
