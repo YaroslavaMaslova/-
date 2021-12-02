f661c4c36fc88cb4acb36bd8fd934ee486a027e5
Ярослава Маслова <yaroslava.maslova.02@mail.ru>
2021-12-02 09:39:34 +0300

    исправлено

--- 1.1.cpp
+++ 1.1.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 #define _USE_MATH_DEFINES // for C++
 #include <cmath>
+double GetA(const double x, const double y, const double z);
+double GetB(const double x, const double y, const double z);
 int main()
 {
-double x, y, z, a, b, e;
- e = 2.718281828459045;
-std::cout << "Введите значение x: "; // просим ввести значения из таблицы//
-std::cin >> x;
-std::cout << "Введите значение y: ";
-std::cin >> y;
-std::cout << "Введите значение z: ";
-std::cin >> z;
-a =y * z *pow(x,2)-z/pow(sin(x/z),2); // находим x и y по формулам //
-b = z * cos(y*x/z)/pow(e,sqrt(z));
-std::cout<<"значение А= " <<a << std::endl<<"значение B= " <<b; //выводим получившиеся значения//
+    const double x = -4.8;
+    const double y = 17.5;
+    const double z = 3.2;
+    const auto a = GetA(x, y, z);
+    const auto b = GetB(x, y, z);
+
+    std::cout << "x = " << x << ", y =  "<< y << ", z = " << z << "\n";
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+
+    return 0;
+}
+
+double GetA (const double x, const double y, const double z)
+{
+    return y * z *pow(x,2)-z/pow(sin(x/z),2);
+}
+
+double GetB (const double x, const double y, const double z)
+{
+    return z * cos(y*x/z)/pow(M_E,sqrt(z));
 }
