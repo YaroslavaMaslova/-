0a8d5ed123405cb41e8cb4332625af31118955ae
Ярослава Маслова <yaroslava.maslova.02@mail.ru>
2021-12-02 12:37:18 +0300

    2,2 исправлено

--- 2.2.cpp
+++ 2.2.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
-#define _USE_MATH_DEFINES // for C++
 #include <cmath>
 int main()
 {
-double number, y, a;
-a = 2.1; //вводим константу, указанную в задаче//
-std::cout<<"введите число "; //просим ввести переменную//
-std::cin >> number;
-if (number < 4 ) //выясняем, что число больше 4//
-{
-y =pow(number,2)+a/number;//да, используем одну формулу//
-}
-else
-{
-y =a*number+pow(number,3);//нет, другую//
-}
-std::cout<<" y = " <<y;//выводим чему равна функция//
+    const double a = 2.1; 
+    std::cout<<"введите число "; 
+    double number;
+    std::cin >> number;
+    double y;
+    if (number < 4 )
+       {
+       y =pow(number,2)+a/number;
+       }
+    else
+       {
+       y =a*number+pow(number,3);
+       }
+    std::cout<<" y = " <<y;
 }
