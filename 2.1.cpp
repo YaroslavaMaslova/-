db1c990f2d419447d39870927d31de2d11de18fa
Ярослава Маслова <yaroslava.maslova.02@mail.ru>
2021-12-16 10:38:26 +0300

    2,1 изменено

--- 2.1.cpp
+++ 2.1.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 #include <cmath>
+enum numbers
+{ 
+    first,
+    second,
+};
+/**
+ * \brief Функция для расчета arithmetic_mean_of_cubes
+ * \param first Параметр first
+ * \param second Параметр second
+ * \return 
+ */
+double Getarithmetic_mean_of_cubes(const double first, const double second);
+/**
+ * \brief Функция для расчета b
+ * \param first Параметр first
+ * \param y Параметр y
+ * \return 
+ */
+double Getaverage_of_modules(const double first, const double second);
+/**
+ * \brief Точка входа в программу
+ * \return Возвращает нуль, в случае успеха
+ */
 int main()
 { 
     std::cout << "Введите первое число: ";
@@ -8,9 +31,18 @@ int main()
     std::cout << "Введите второе число: ";
     double second;
     std::cin >> second;
-    double arithmetic_mean_of_cubes = (pow(first,3)+pow(second,3))/2;
-    first = fabs(first);
-    second = fabs(second);
-    double average_of_modules =(first + second)/2;//находим среднее число модулей //
+    double z, x;
+    z = fabs(first);
+    x = fabs(second);
+    const auto arithmetic_mean_of_cubes = Getarithmetic_mean_of_cubes(first, second);
+    const auto average_of_modules = Getaverage_of_modules(first, second);
     std::cout<<"среднее арифметическое кубов этих чисел =" <<arithmetic_mean_of_cubes<< std::endl<<"среднее геометрическое модулей этих чисел " <<average_of_modules;
+}
+double Getarithmetic_mean_of_cubes(const double first, const double second);
+{
+    return (pow(first,3)+pow(second,3))/2;
+}
+double Getaverage_of_modules (const double x, const double z)
+{
+    return (z + x)/2;
 }
