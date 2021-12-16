34868ccef9fed9dd6cf1d00f772efe2effe245d2
Ярослава Маслова <yaroslava.maslova.02@mail.ru>
2021-12-16 11:06:10 +0300

    3,3 изменено

--- 3.3.cpp
+++ 3.3.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 #include <cmath>
+/**
+ * \brief Функция для расчета y
+ * \param x Параметр x
+ * \return 
+ */
+double Gety(const double x);
+/**
+ * \brief Функция для расчета S
+ * \param S Параметр S
+ * \param n Параметр n
+ * \param z Параметр z
+ * \param x Параметр x
+ * \return 
+ */
+double GetS(const double S, const double n, const double z,const double x);
+/**
+ * \brief Точка входа в программу
+ * \return Возвращает нуль, в случае успеха
+ */
 int main()
 { 
     double e=pow(30,-4);
@@ -9,11 +28,19 @@ int main()
     double x = 0.1;
     while (x <= 1)
         {
-        double y = (pow(x,2)/4+x/2+1);
+        double y = Gety(x);
         z = z * n;
-        double S =S+((pow(n,2)+1)/z)*pow(x/2,n);
+        double S =GetS(S, n, z, x);
         std::cout  << x << " " << y << " " << S << std::endl;
         n= n +1;
         x = x+e;
 }
+}
+double Gety(const double x)
+{ 
+    return (pow(x,2)/4+x/2+1);
+}
+double GetS(const double S, const double n, const double z,const double x)
+{
+    return S+((pow(n,2)+1)/z)*pow(x/2,n);
 }
