d2ef9aef2948cb965e03386c9520495925a5dcd0
Ярослава Маслова <yaroslava.maslova.02@mail.ru>
2021-12-16 09:48:24 +0300

    1.1 изменено

--- 1.1.cpp
+++ 1.1.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
-#define _USE_MATH_DEFINES // for C++
 #include <cmath>
+/**
+ * \brief Функция для расчета a
+ * \param x Параметр x
+ * \param y Параметр y
+ * \param z Параметр z
+ * \return 
+ */
 double GetA(const double x, const double y, const double z);
+/**
+ * \brief Функция для расчета b
+ * \param x Параметр x
+ * \param y Параметр y
+ * \param z Параметр z
+ * \return 
+ */
 double GetB(const double x, const double y, const double z);
+/**
+ * \brief Точка входа в программу
+ * \return Возвращает нуль, в случае успеха
+ */
 int main()
 {
     const double x = -4.8;
@@ -24,5 +41,5 @@ double GetA (const double x, const double y, const double z)
 
 double GetB (const double x, const double y, const double z)
 {
-    return z * cos(y*x/z)/pow(M_E,sqrt(z));
+    return z * cos(y*x/z)/exp(sqrt(z));
 }
